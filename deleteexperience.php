22c0825b067f7b2dc7543e1c43d8cdc319f33ee0
ayoubjemali1 <ayoubjemali99@gmail.com>
2024-12-10 19:31:21 +0100

    Delete deleteexperience.php

--- deleteexperience.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../../controller/ExperienceController.php';
-$expC = new ExperienceController();
-$expC->deleteexperience($_GET["id_exp"]);
-header('Location:experienceList.php');
-?>
